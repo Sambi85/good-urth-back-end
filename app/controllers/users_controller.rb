40a2b6bdc0f5eea225e40540ffb6d8049f4608f7
github username <github email address>
2020-11-09 15:12:59 -0500

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
         user = User.create(user_name)
         render :json => user
     end
+    
 
     # def profile
     #     render json: { user: UserSerializer.new(current_user) }, status: accepted
